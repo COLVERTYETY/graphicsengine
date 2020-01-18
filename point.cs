55f95826ced03236571434334a1bbdf1a46406ea
COLVERTYETY <niconoob1@gmail.com>
2020-01-18 01:03:35 +0100

    basic constructur

--- point.cs
+++ point.cs
@@ -1,6 +1,6 @@
 namespace graphicsengine
 {
-    class point{
+    class point{///should this be a struct?
         public float x;
         public float y;
         public point(float xx,float yy){
