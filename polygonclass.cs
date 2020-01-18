bcc996ffce4783792e01e6e3ef199be801b1d3ac
COLVERTYETY <niconoob1@gmail.com>
2020-01-18 01:03:51 +0100

    constructors

---
+++ polygonclass.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+namespace graphicsengine
+{
+    class polygon{
+        public List<line> lines;
+        public point [] points;
+        public polygon(point [] pointarray){
+            points = pointarray;
+            for(int i = 0; i < (points.Length-1);i++){
+                lines.Add(new line(points[i],points[i+1]));
+            }
+        }
+        public polygon(int radius, int n){///regular polygon
+            points = new point[n];
+            int index=0;
+            for(double i=0.0F;i<(2*Math.PI);i+=((2*Math.PI)/(Convert.ToDouble(n)))){
+                float x = (float)( radius * Math.Cos(i));
+                float y = (float)( radius * Math.Sin(i));
+                index++;
+                points[index]=new point(x,y);
+            }
+            for(int i = 0; i < (points.Length-1);i++){
+                lines.Add(new line(points[i],points[i+1]));
+            }
+        }
+    }
+}
