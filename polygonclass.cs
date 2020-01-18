69a1049967b131507249a18f3f2ff3ce6cb669a7
COLVERTYETY <niconoob1@gmail.com>
2020-01-18 16:37:46 +0100

    added offset

--- polygonclass.cs
+++ polygonclass.cs
@@ -13,14 +13,14 @@ namespace graphicsengine
             }
             allthePolygon.Add(this);
         }
-        public polygon(int radius, int n){///regular polygon //still requires offset implementation
+        public polygon(int radius, int n , int offsetx , int offsety){///regular polygon //still requires offset implementation
             points = new point[n];
             int index=0;
             for(double i=0.0F;i<(2*Math.PI);i+=((2*Math.PI)/(Convert.ToDouble(n)))){
                 float x = (float)( radius * Math.Cos(i));
                 float y = (float)( radius * Math.Sin(i));
                 index++;
-                points[index]=new point(x,y);
+                points[index]=new point(x + offsetx ,y + offsety);
             }
             for(int i = 0; i < (points.Length-1);i++){
                 lines.Add(new line(points[i],points[i+1]));
