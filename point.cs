92eb58ebdc3fd1857eb20379ab631adf54e0fec6
COLVERTYETY <niconoob1@gmail.com>
2020-01-18 01:03:18 +0100

    basic constructor

---
+++ point.cs
@@ -0,0 +1,11 @@
+namespace graphicsengine
+{
+    class point{
+        public float x;
+        public float y;
+        public point(float xx,float yy){
+            x=xx;
+            y=yy;
+        }
+    }
+}
