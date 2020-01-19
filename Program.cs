1c3969d5da5369fa70168fa33f774cbafcec5649
COLVERTYETY <niconoob1@gmail.com>
2020-01-19 21:44:46 +0100

    hor not ok

--- Program.cs
+++ Program.cs
@@ -16,7 +16,22 @@ namespace graphicsengine
             frame.fullflip();
             Console.ReadLine();
             frame.Clear();
-            polygon second  =  new polygon(7,4,10,10);  // Noncompliant
+            line firstline = new line(new point(5,5),new point(5,10),'|');
+            line secondline = new line(new point(5,10),new point(10,10),'-');
+            line thirdline = new line(new point(10,10),new point(10,5),'|');
+            line fourthline = new line(new point(10,5),new point(5,5),'-');
+            line diagonal  = new line(new point(0,0),new point(10,10),'\\');
+            line seconddiagonal  = new line(new point(0,10),new point(10,0),'/');
+            frame.renderline(firstline);
+            frame.renderline(secondline);
+            frame.renderline(thirdline);
+            frame.renderline(fourthline);
+            frame.renderline(diagonal);
+            frame.renderline(seconddiagonal);
+            frame.fullflip();
+            Console.ReadLine();
+            frame.Clear();
+            new polygon(5,3,10,10);  // Noncompliant
             frame.renderpolygons();
             frame.fullflip();
             Console.ReadLine();
