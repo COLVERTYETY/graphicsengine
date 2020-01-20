32ae78affc90a1a345d14f27151ee85ec68c4e90
COLVERTYETY <niconoob1@gmail.com>
2020-01-20 13:59:37 +0100

    it works!!

--- polygonclass.cs
+++ polygonclass.cs
@@ -17,18 +17,24 @@ namespace graphicsengine
         public polygon(int radius, int n , double offsetx , double offsety, bool numbered = false){///regular polygon 
         
             points.Clear();
-            for(int i=0;i<(n-1);i++){
+            for(int i=0;i<(n);i++){
                 double x = (double)( radius * Math.Cos((2*i*Math.PI)/n));
                 double y = (double)( radius * Math.Sin((2*i*Math.PI)/n));
                 points.Add(new point(x + offsetx ,y + offsety));
             }
+            line temp;
             for(int i = 0; i < (points.Count-1);i++){
-                line temp = (new line(points[i],points[i+1]));
+                temp = (new line(points[i],points[i+1]));
                 if(numbered){
-                    temp.spcecialchar=Convert.ToChar(Convert.ToString(i));
+                    temp.spcecialchar=Convert.ToChar(Convert.ToString(i));  
                 }
                 lines.Add(temp);
             }
+            temp = (new line(points[0],points[points.Count-1]));
+                if(numbered){
+                    temp.spcecialchar=Convert.ToChar(Convert.ToString(points.Count-1));  
+                }
+                lines.Add(temp);
             allthePolygon.Add(this);
         }
     }
